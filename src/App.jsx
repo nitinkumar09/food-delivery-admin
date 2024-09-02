d8cbf34aa59f593a48378b617cec310e90bf23e5
nitinkumar09 <kumarnitin83086@gmail.com>
2024-09-02 13:11:49 +0530

    ho gya

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Orders from './pages/Orders/Orders'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
-  const url = "https://food-delivery-backend-s33v.onrender.com"
+  const url = "https://food-delivery-backend-dwal.onrender.com"
   return (
     <div>
       <ToastContainer />
